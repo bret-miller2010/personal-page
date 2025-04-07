589fde453826188bad000784e79dc17e0491cf3e
Bret Miller <bret.miller2015@gmail.com>
2025-04-07 14:19:24 -0500

    updates

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -6,7 +6,7 @@ export default function AboutSection() {
         <>
             <div className="w-1/2 flex justify-around items-center mt-20">
                 <div className="w-1/2 h-fit p-4 m-2 bg-gray-100 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out border-2 border-black">
-                    Hey, I’m Bret — a software developer with a degree in Computer Engineering from Michigan Technological University. Tech has always been a big part of my life. I grew up gaming, got hooked on how
+                    Hey, I’m Bret! A software developer with a degree in Computer Engineering from Michigan Technological University. Tech has always been a big part of my life. I grew up gaming, got hooked on how
                     things work, and turned that curiosity into a career I love.
                     <br />
                     When I’m not coding, you’ll usually find me staying active, gaming with friends, or diving into a new side project just for fun.
