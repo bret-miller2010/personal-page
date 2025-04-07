324dd4a5fe549b82b1fb0354fb4f290c936aede5
Bret Miller <bret.miller2015@gmail.com>
2025-04-07 14:01:20 -0500

    build changes

--- src/components/SkillSection/SkillIcon.tsx
+++ src/components/SkillSection/SkillIcon.tsx
@@ -1,6 +1,11 @@
 /** @format */
 
-function SkillIcon({ skill, image }) {
+type SkillIconProps = {
+    skill: string;
+    image: string;
+};
+
+function SkillIcon({ skill, image }: SkillIconProps) {
     return (
         <div className="flex flex-col items-center justify-center p-4 m-2 bg-gray-100 border-2 border-black rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
             <img
